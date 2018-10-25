398d541a4d2eb99d437cf8ac2ad58e2fc42a462b
azwickey-pivotal <azwickey@pivotal.io>
2018-10-25 08:43:03 -0400

    updated settings for post-install validation

--- tasks/config-harbor-nats/task.sh
+++ tasks/config-harbor-nats/task.sh
@@ -81,121 +81,124 @@ INTERNAL_HARBOR_APP_IP=$(om-linux \
 echo "Discovered Harbor App running at: $INTERNAL_HARBOR_APP_IP!!"
 echo ""
 
-echo "Going to create NAT entry between External Address: $HARBOR_APP_DOMAIN_IP and Harbor App Internal IP: $INTERNAL_HARBOR_APP_IP"
-echo "   on T0Router: $PKS_T0_ROUTER_NAME on NSX Manager: $NSX_API_MANAGER"
-echo ""
+if [ "$NSX_ENABLED" == "true" ]; then
 
-export PKS_T0_ROUTER_ID=$(curl \
-      -k -u "$NSX_API_USER:$NSX_API_PASSWORD" \
-      https://$NSX_API_MANAGER:443/api/v1/logical-routers \
-      2>/dev/null \
-      | jq -r --arg search_name $PKS_T0_ROUTER_NAME '.results[] | select ( .display_name | contains($search_name) ) | .id ' )
-if [ "$PKS_T0_ROUTER_ID" == "" -o "$PKS_T0_ROUTER_ID" == "null" ]; then
-  echo "Error: Unable to find T0 Router with name: $PKS_T0_ROUTER_NAME in NSX Mgr"
-  echo "Exiting !!"
-  exit 1
-fi
+  echo "Going to create NAT entry between External Address: $HARBOR_APP_DOMAIN_IP and Harbor App Internal IP: $INTERNAL_HARBOR_APP_IP"
+  echo "   on T0Router: $PKS_T0_ROUTER_NAME on NSX Manager: $NSX_API_MANAGER"
+  echo ""
 
-dnat_rule_payload=$( jq -n \
-  --arg internal_harbor_app_ip $INTERNAL_HARBOR_APP_IP \
-  --arg harbor_app_domain_ip $HARBOR_APP_DOMAIN_IP \
-  '
-  {
-    "resource_type": "NatRule",
-    "enabled" : true,
-    "rule_priority": 1001,
-    "action": "DNAT",
-    "match_destination_network": $harbor_app_domain_ip,
-    "translated_network" : $internal_harbor_app_ip
-  }
-  '
-)
+  export PKS_T0_ROUTER_ID=$(curl \
+        -k -u "$NSX_API_USER:$NSX_API_PASSWORD" \
+        https://$NSX_API_MANAGER:443/api/v1/logical-routers \
+        2>/dev/null \
+        | jq -r --arg search_name $PKS_T0_ROUTER_NAME '.results[] | select ( .display_name | contains($search_name) ) | .id ' )
+  if [ "$PKS_T0_ROUTER_ID" == "" -o "$PKS_T0_ROUTER_ID" == "null" ]; then
+    echo "Error: Unable to find T0 Router with name: $PKS_T0_ROUTER_NAME in NSX Mgr"
+    echo "Exiting !!"
+    exit 1
+  fi
 
-snat_rule_payload=$( jq -n \
-  --arg internal_harbor_app_ip $INTERNAL_HARBOR_APP_IP \
-  --arg harbor_app_domain_ip $HARBOR_APP_DOMAIN_IP \
-  '
-  {
-    "resource_type": "NatRule",
-    "enabled" : true,
-    "rule_priority": 1001,
-    "action": "SNAT",
-    "match_source_network": $internal_harbor_app_ip,
-    "translated_network" : $harbor_app_domain_ip
-  }
-  '
-)
+  dnat_rule_payload=$( jq -n \
+    --arg internal_harbor_app_ip $INTERNAL_HARBOR_APP_IP \
+    --arg harbor_app_domain_ip $HARBOR_APP_DOMAIN_IP \
+    '
+    {
+      "resource_type": "NatRule",
+      "enabled" : true,
+      "rule_priority": 1001,
+      "action": "DNAT",
+      "match_destination_network": $harbor_app_domain_ip,
+      "translated_network" : $internal_harbor_app_ip
+    }
+    '
+  )
 
-insert_dnat_rule=1
-insert_snat_rule=1
+  snat_rule_payload=$( jq -n \
+    --arg internal_harbor_app_ip $INTERNAL_HARBOR_APP_IP \
+    --arg harbor_app_domain_ip $HARBOR_APP_DOMAIN_IP \
+    '
+    {
+      "resource_type": "NatRule",
+      "enabled" : true,
+      "rule_priority": 1001,
+      "action": "SNAT",
+      "match_source_network": $internal_harbor_app_ip,
+      "translated_network" : $harbor_app_domain_ip
+    }
+    '
+  )
 
-existing_nat_rules_json=$(curl -k -u "$NSX_API_USER:$NSX_API_PASSWORD" \
-        https://$NSX_API_MANAGER:443/api/v1/logical-routers/${PKS_T0_ROUTER_ID}/nat/rules)
-#echo "Full existing rules: $existing_nat_rules_json"
+  insert_dnat_rule=1
+  insert_snat_rule=1
 
-number_of_rules=$( echo $existing_nat_rules_json | jq '.result_count' )
-echo number_of_rules rules: $number_of_rules
-max_index=$(expr $number_of_rules - 1)
-for index in $(seq 0 $max_index )
-do
-  nat_rule=$( echo $existing_nat_rules_json | jq --argjson index $index '.results[$index]' )
-  #echo "Nat rule: $nat_rule"
+  existing_nat_rules_json=$(curl -k -u "$NSX_API_USER:$NSX_API_PASSWORD" \
+          https://$NSX_API_MANAGER:443/api/v1/logical-routers/${PKS_T0_ROUTER_ID}/nat/rules)
+  #echo "Full existing rules: $existing_nat_rules_json"
 
-  translated_ip=$(echo $nat_rule | jq -r .translated_network )
+  number_of_rules=$( echo $existing_nat_rules_json | jq '.result_count' )
+  echo number_of_rules rules: $number_of_rules
+  max_index=$(expr $number_of_rules - 1)
+  for index in $(seq 0 $max_index )
+  do
+    nat_rule=$( echo $existing_nat_rules_json | jq --argjson index $index '.results[$index]' )
+    #echo "Nat rule: $nat_rule"
 
-  nat_rule_type=$( echo $nat_rule | jq -r .action )
+    translated_ip=$(echo $nat_rule | jq -r .translated_network )
 
-  if [ "$translated_ip" == "$INTERNAL_HARBOR_APP_IP" -o "$translated_ip" == "$HARBOR_APP_DOMAIN_IP" ]; then
+    nat_rule_type=$( echo $nat_rule | jq -r .action )
 
-    if [ "$nat_rule_type" == "DNAT" ]; then
-      match=$(check_match_in_nat_rules "$nat_rule" "$dnat_rule_payload")
-      if [ "$match" == "0" ]; then
-        #echo "Found Match for dnat rule: $nat_rule"
-        insert_dnat_rule=0
-      fi
-    else
-      match=$(check_match_in_nat_rules "$nat_rule" "$snat_rule_payload")
-      if [ "$match" == "0" ]; then
-        #echo "Found Match for snat rule: $nat_rule"
-        insert_snat_rule=0
+    if [ "$translated_ip" == "$INTERNAL_HARBOR_APP_IP" -o "$translated_ip" == "$HARBOR_APP_DOMAIN_IP" ]; then
+
+      if [ "$nat_rule_type" == "DNAT" ]; then
+        match=$(check_match_in_nat_rules "$nat_rule" "$dnat_rule_payload")
+        if [ "$match" == "0" ]; then
+          #echo "Found Match for dnat rule: $nat_rule"
+          insert_dnat_rule=0
+        fi
+      else
+        match=$(check_match_in_nat_rules "$nat_rule" "$snat_rule_payload")
+        if [ "$match" == "0" ]; then
+          #echo "Found Match for snat rule: $nat_rule"
+          insert_snat_rule=0
+        fi
       fi
     fi
+
+  done
+
+  if [ $insert_dnat_rule -ne 0 ]; then
+    dnat_output=$(curl -k -u "$NSX_API_USER:$NSX_API_PASSWORD" \
+          https://$NSX_API_MANAGER:443/api/v1/logical-routers/${PKS_T0_ROUTER_ID}/nat/rules \
+          -X POST \
+          -H 'Content-type: application/json' \
+          -d "$dnat_rule_payload")
+    if [ "$?" != 0 ]; then
+      echo "Problem in creating DNAT entry: $dnat_output"
+      exit 1
+    fi
+  else
+    echo "Found existing dnat rule that matches already, skipping rule creation in NATs on T0Router"
   fi
 
-done
-
-if [ $insert_dnat_rule -ne 0 ]; then
-  dnat_output=$(curl -k -u "$NSX_API_USER:$NSX_API_PASSWORD" \
-        https://$NSX_API_MANAGER:443/api/v1/logical-routers/${PKS_T0_ROUTER_ID}/nat/rules \
-        -X POST \
-        -H 'Content-type: application/json' \
-        -d "$dnat_rule_payload")
-  if [ "$?" != 0 ]; then
-    echo "Problem in creating DNAT entry: $dnat_output"
-    exit 1
+  if [ $insert_snat_rule -ne 0 ]; then
+    snat_output=$(curl -k -u "$NSX_API_USER:$NSX_API_PASSWORD" \
+          https://$NSX_API_MANAGER:443/api/v1/logical-routers/${PKS_T0_ROUTER_ID}/nat/rules \
+          -X POST \
+          -H 'Content-type: application/json' \
+          -d "$snat_rule_payload")
+    if [ "$?" != 0 ]; then
+      echo "Problem in creating SNAT entry: $snat_output"
+      exit 1
+    fi
+  else
+    echo "Found existing snat rule that matches already, skipping rule creation in NATs on T0Router"
   fi
-else
-  echo "Found existing dnat rule that matches already, skipping rule creation in NATs on T0Router"
+
+  echo ""
+  echo "DNS Entry of ${HARBOR_APP_FQDN} expected to resolve External IP: $HARBOR_APP_DOMAIN_IP"
+  echo "This ip should be routable via the T0 Router $PKS_T0_ROUTER_NAME" on $NSX_API_MANAGER
+  echo ""
+  echo "Created NAT rules for Harbor App API IP $INTERNAL_HARBOR_APP_IP to be accessible from external IP: $HARBOR_APP_DOMAIN_IP"
+  echo "Note: Do a sanity check of the NAT rules in NSX Manager and delete any duplicate or older/wrong entries!!"
+  echo ""
 fi
-
-if [ $insert_snat_rule -ne 0 ]; then
-  snat_output=$(curl -k -u "$NSX_API_USER:$NSX_API_PASSWORD" \
-        https://$NSX_API_MANAGER:443/api/v1/logical-routers/${PKS_T0_ROUTER_ID}/nat/rules \
-        -X POST \
-        -H 'Content-type: application/json' \
-        -d "$snat_rule_payload")
-  if [ "$?" != 0 ]; then
-    echo "Problem in creating SNAT entry: $snat_output"
-    exit 1
-  fi
-else
-  echo "Found existing snat rule that matches already, skipping rule creation in NATs on T0Router"
-fi
-
-echo ""
-echo "DNS Entry of ${HARBOR_APP_FQDN} expected to resolve External IP: $HARBOR_APP_DOMAIN_IP"
-echo "This ip should be routable via the T0 Router $PKS_T0_ROUTER_NAME" on $NSX_API_MANAGER
-echo ""
-echo "Created NAT rules for Harbor App API IP $INTERNAL_HARBOR_APP_IP to be accessible from external IP: $HARBOR_APP_DOMAIN_IP"
-echo "Note: Do a sanity check of the NAT rules in NSX Manager and delete any duplicate or older/wrong entries!!"
-echo ""
